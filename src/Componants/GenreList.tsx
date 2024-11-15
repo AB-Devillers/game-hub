13caffb03af592ee8a35a53ee5312d5b6510683d
Abdullah <abdullahgr9301@gmail.com>
2024-11-15 20:11:11 +0500

    Adding Geners Heading

--- src/Componants/GenreList.tsx
+++ src/Componants/GenreList.tsx
@@ -12,6 +12,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: props) => {
   const genre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <>
+      <h1 className="text-white font-bold text-3xl mb-4">Geners</h1>
       {loading && genre.map((gen) => <GenreSkeleton key={gen} />)}
       {error && <p className="text-red-600">{error}</p>}
       <ul>
